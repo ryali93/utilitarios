f947fcc50d629e5a448d715d7db3518b2b175fe9
Roy Yali <ryali93@gmail.com>
2019-06-24 15:03:37 -0500

    Servicio de descarga de imagenes a partir de servicio ArcGis

--- downloadImg.py
+++ downloadImg.py
@@ -1,16 +1,11 @@
 import os, requests
 import json, uuid
 import urllib2
-# token = 'rpt_Zd9ooapP-6AbayixwQvCcTs16Y5FAx5b6a-C6msEZKWyrjcUNvKmNF8KhyDVwL3KZoLO45IsLuIbmGobqNgem7i6Jk8xUX9SMvKtPF9dhPlX4zdp5Y44NvCBtMc3d79U0SGiW8enUq704UqeF4mEJ1HWAJHUGGFQGWfg-B4fm8uW_LqIXVnVPhjvqJR3g3k-F1hHaEB51-2L8wOzRtYJIAetQ2CAe4WLJ6joizaNSDOP9_dOLV5NxSD-VZV9eNWISVsVsm6SqLaFQMY4hazy8YzrdBtyVr1ppdbiImw.'
-# 'https://services1.arcgis.com/IOnDXYLCAWAfoO54/ArcGIS/rest/services/service_407c8304a8c040b0806b3e422fb0f097/FeatureServer/8/33/attachments/26?token='
 
 _USER = 'publicadorapp'
 _PASSWORD = 'publ1c4d0r'
 
 def getToken():
-    '''
-    :return: Devuelve el token unico por modulo y maquina 
-    '''
     _URL = "https://www.arcgis.com/sharing/generateToken"
     data = {
         'username': _USER,
@@ -25,8 +20,11 @@ def getToken():
     return token
 
 token = getToken()
-# Servicio de fotos
+
+
 url = 'https://services1.arcgis.com/IOnDXYLCAWAfoO54/ArcGIS/rest/services/service_407c8304a8c040b0806b3e422fb0f097/FeatureServer/8'
+IDENTIFICADOR = "FICHA_FT"
+PATH = r"D:\RYali\TDR5\2product\imgs"
 
 class downloadImg(object):
     def __init__(self):
@@ -50,7 +48,7 @@ class downloadImg(object):
         print("\n")
         listImg = []
         for f in res["features"]:
-            listImg.append([f["attributes"]["objectid"], f["attributes"]["FICHA_FT"]])
+            listImg.append([f["attributes"]["objectid"], f["attributes"][IDENTIFICADOR]])
         return listImg
 
     def readUrlPhotos(self):
@@ -72,14 +70,11 @@ class downloadImg(object):
                 url_img = os.path.join(self.url, str(poi), "attachments", "{}?token={}".format(fid, token))
                 listaUrlImg.append([poi, fid ,url_img])
         return listaUrlImg                
-        # filedata = urllib2.urlopen(url_img)
-        # image = filedata.read()
-        # row = [image, x[2], x[3], x[0], x[1]]
 
     def readImg(self, url_img, path):
         filedata = urllib2.urlopen(url_img)
         image = filedata.read()
-        img = open(os.path.join(r"D:\RYali\TDR5\2product\imgs","{}.png").format(path), "wb")
+        img = open(os.path.join(PATH,"{}.png").format(path), "wb")
         img.write(image)
         img.close()
 
